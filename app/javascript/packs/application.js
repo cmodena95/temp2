4b96343c24db2e8a51b80f931b5111973a3cbc85
Camila Modena <itscamilla@hotmail.com>
2022-04-26 16:31:41 +0200

    resolve

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "controllers"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "controllers"
 import "bootstrap"
