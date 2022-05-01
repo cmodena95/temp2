2084913ceb2616e488a78b8d9ed9ef780d8c578e
Camila Modena <itscamilla@hotmail.com>
2022-05-01 13:15:06 +0200

    header, subheader + title editable

--- app/javascript/controllers/edit_controller.js
+++ app/javascript/controllers/edit_controller.js
@@ -1,11 +1,19 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("!!!")
-  }
+  static targets = ["header", "editHeaderBox", "subheader", "editSubheaderBox", "title", "editTitleBox"]
 
-  edit() {
-    
+  edit(event) {
+    event.currentTarget.classList.add("d-none")
+    if (event.currentTarget.id == "header") {
+      this.editHeaderBoxTarget.classList.remove("d-none")
+      this.headerTarget.classList.add("d-none")
+    } else if (event.currentTarget.id == "subheader") {
+      this.editSubheaderBoxTarget.classList.remove("d-none")
+      this.subheaderTarget.classList.add("d-none")
+    } else if (event.currentTarget.id == "title") {
+      this.editTitleBoxTarget.classList.remove("d-none")
+      this.titleTarget.classList.add("d-none")
+    }
   }
 }
